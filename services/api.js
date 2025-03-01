cb79f917b252fc087d2e7851816b3aa4a99f5063
Votre Nom <Vous@exemple.com>
2025-03-02 00:24:13 +0100

    build op

--- services/api.js
+++ services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://10.188.192.249:8080/', // Remplacez par l'URL de votre backend
 
     baseURL: 'http://localhost:8080/', // Remplacez par l'URL de votre backend
 
