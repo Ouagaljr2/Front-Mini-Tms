a234c3af5caa5d4097e5debb415dc888a28cc78d
Votre Nom <Vous@exemple.com>
2025-03-03 12:50:35 +0100

    adresse ip fac

--- services/api.js
+++ services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
 
-    baseURL: 'http://10.192.12.42:8080/', // Remplacez par l'URL de votre backend
+    //baseURL: 'http://10.192.12.42:8080/', // Remplacez par l'URL de votre backend
+    baseURL: 'http://10.100.18.111:8080/', // Remplacez par l'URL de votre backend
 
     //baseURL: 'http://localhost:8080/', // Pour faire marcher sur android studio il faut mettre l'adresse ip de votre machine
 
